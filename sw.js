c3663600c1b0efbb8bce77f0e0d3d7b24b1c221c
xatusbetazx17 <xatusbetazx17@gmail.com>
2025-10-17 22:05:38 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,14 @@
-// sw.js (replace CACHE_NAME and add new URLs)
-const CACHE_NAME = 'math-v4';
+// sw.js
+const CACHE_NAME = 'ma025b-v8'; // <-- bump this number
 const ASSETS = [
   './',
   './index.html',
-  './algebra_quiz_notebook.html',
-  './algebra_units6_9_quiz.html',
+  './practice_exam1.html',
+  './random_practice.html',
   './unit5_quick_review.html',
   './unit5_polynomials_quiz.html',
-  './practice_exam1.html',      // NEW
-  './random_practice.html',     // NEW
+  './algebra_quiz_notebook.html',
+  './algebra_units6_9_quiz.html',
   './references.html'
 ];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+// ...rest of your SW stays the same
